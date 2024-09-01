b46139b28a3605a3f1c330cb7c43a4d813387623
nguyenvhoan <22dh114532@st.huflit.edu.vn>
2024-09-01 21:30:31 +0700

    fsdfdfdfsd

---
+++ booking/lib/app/my_app.dart
@@ -0,0 +1,15 @@
+import 'package:booking/Management/Dashboard.dart';
+import 'package:booking/app/adminManager.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Adminmanager(),
+    );
+  }
+}
